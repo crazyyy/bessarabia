84e2afcdf42be486538bf22d023edf8540526778
crazyyy <crazyyy@gmail.com>
2016-02-06 08:38:21 +0200

    frontpageready

--- wordpress/wp-content/themes/wp-bessarabia/front-page.php
+++ wordpress/wp-content/themes/wp-bessarabia/front-page.php
@@ -1,27 +1,30 @@
 <?php /* Template Name: Home Page */ get_header(); ?>
   <?php get_sidebar('left'); ?>
-  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
     <div id="post-<?php the_ID(); ?>" <?php post_class('article-left-sidebar'); ?>>
 
-      <article id="post-<?php the_ID(); ?>" <?php post_class('front-loop looper'); ?>>
-        <a rel="nofollow" class="feature-img" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-          <?php if ( has_post_thumbnail()) :
-            the_post_thumbnail('medium');
-          else: ?>
-            <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
-          <?php endif; ?>
-          <span class="cat"><?php the_category(', '); ?></span>
-        </a><!-- /post thumbnail -->
-        <h2 class="inner-title">
-          <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-        </h2><!-- /post title -->
-        <span class="date"><span><?php the_time('G:i'); ?></span>, <?php the_time('j F Y'); ?></span>
-      </article><!-- /looper -->
+      <?php query_posts("showposts=10"); ?>
+        <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
+            <article id="post-<?php the_ID(); ?>" <?php post_class('front-loop looper'); ?>>
+              <a rel="nofollow"href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                <?php if ( has_post_thumbnail()) :
+                  the_post_thumbnail('medium');
+                else: ?>
+                  <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+                <?php endif; ?>
+                <?php
+                  $category = get_the_category();
+                  if ($category) {
+                    echo '<span class="cat catid-'. $category[0]->slug .'">' . $category[0]->name .'</span> ';
+                  }
+                ?>
+                <h2 class="inner-title"><?php the_title(); ?></h2><!-- /post title -->
+                <span class="date"><span><?php the_time('G:i'); ?></span>, <?php the_time('j F Y'); ?></span>
+              </a>
+            </article><!-- /looper -->
+
+        <?php endwhile; endif; ?>
+      <?php wp_reset_query(); ?>
     </div><!-- article-left-sidebar -->
-  <?php endwhile; else: // If 404 page error ?>
-    <div class="article-left-sidebar">
-      <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
-    </div>
-  <?php endif; ?>
 <?php get_footer(); ?>

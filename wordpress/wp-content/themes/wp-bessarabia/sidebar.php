2acbcb092dfa6dacf07c76b8778c6cd5cdc7459d
crazyyy <crazyyy@gmail.com>
2016-02-06 09:59:59 +0200

    nesidebars

--- wordpress/wp-content/themes/wp-bessarabia/sidebar.php
+++ wordpress/wp-content/themes/wp-bessarabia/sidebar.php
@@ -1,4 +1,30 @@
-<aside class="sidebar" role="complementary">
+<aside class="sidebar sidebar-right" role="complementary">
+
+  <div class="widget widget-cat-list widget-cat-list-orange">
+    <h6>Новости региона</h6>
+    <ul>
+      <?php query_posts("showposts=4&cat=1"); ?>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <li>
+          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </li>
+        <?php endwhile; endif; ?>
+      <?php wp_reset_query(); ?>
+    </ul>
+  </div><!-- /.widget widget-cat-list widget-cat-list-orange -->
+
+  <div class="widget widget-cat-list widget-cat-list-blue">
+    <h6>Новости рубрики</h6>
+    <ul>
+      <?php query_posts("showposts=4&cat=9"); ?>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <li>
+          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </li>
+        <?php endwhile; endif; ?>
+      <?php wp_reset_query(); ?>
+    </ul>
+  </div><!-- /.widget widget-cat-list widget-cat-list-blue -->
 
   <?php if ( is_active_sidebar('widgetarea2') ) : ?>
     <?php dynamic_sidebar( 'widgetarea2' ); ?>
